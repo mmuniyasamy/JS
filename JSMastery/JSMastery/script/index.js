e5dd4af116e62f67b8af53a1fb61d888882756ab
mmuniyasamy <mmuniyasamy@hotmail.com>
2018-04-10 14:09:40 -0500

    Module Pattern Using Closure

--- JSMastery/JSMastery/script/index.js
+++ JSMastery/JSMastery/script/index.js
@@ -1,1 +1,29 @@
-﻿console.log("Hello world")
+﻿function User() {
+    var firstName;
+    var lastName;
+
+    function login(fname, lname) {
+        firstName = fname;
+        lastName = lname;
+    }
+
+    function printLoginDetails() {
+        console.log(firstName);
+        console.log(lastName);
+    };
+
+    var loginPublicAPI = {
+        doLogin: login,
+        printDetails: printLoginDetails
+    }
+
+    return loginPublicAPI;
+}
+
+muthu = User();
+muthu.doLogin("Muthukumar", "Muniyasamy");
+muthu.printDetails();
+
+venky = new User();
+muthu.doLogin("Venky", "Pablo");
+muthu.printDetails();
